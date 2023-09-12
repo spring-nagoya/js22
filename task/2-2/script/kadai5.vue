b63928abf7038b59b0f28addf41d92295dafa104
taku10101 <taku1011@gmail.com>
2023-09-12 13:42:00 +0900

    kadai06

---
+++ task/2-2/script/kadai5.vue
@@ -0,0 +1,23 @@
+
+<!-- html側の表示(今回は未入力) -->
+<template>
+    <div>
+        <div class="error">未入力</div>
+    </div>
+</template>
+
+<!-- script関連の記述 -->
+<script>
+    module.exports = {
+        // vueのname定義(今回は'kadai5.vue')
+        name: 'kadai5'
+    }
+
+</script>
+
+<style scoped>
+
+    .error{
+        color: #ff00ff;
+    }
+</style>
--- task/2-2/script/r05comp.vue
+++ task/2-2/script/r05comp.vue
@@ -1,28 +1,31 @@
+<!-- htmlの表示に関わること関連 -->
 <template>
-  <div>
-    <div class="divTitle">r05comp.vueです。</div>
-    <div class="divMsg">{{ message }}</div>
-  </div>
+    <div>
+        <div class="divTitle">r05comp.vueです。</div>
+        <div class="divMsg">{{ message }}</div>
+    </div>
 </template>
 
+<!-- script関連 -->
 <script>
-module.exports = {
-  data: function () {
-    return {
-      message: "片岡",
-    };
-  },
-};
+    module.exports = {
+        data: function(){
+            return {
+                message: "片岡先生"
+            }
+        }
+    }
 </script>
 
+<!-- css関連 -->
 <style>
-.divTitle {
-  color: #ff0000;
-}
+    .divTitle{
+        /* 文字色を赤に */
+        color: #ff0000;
+    }
 </style>
-
 <style scoped>
-.divMsg {
-  color: #00ff00;
-}
+    .divMsg{
+        color: #00ff00;
+    }
 </style>
