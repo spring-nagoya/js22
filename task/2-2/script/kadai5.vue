42ccab7d31ef807224feda79ac58f831d11cdbf8
taku10101 <taku1011@gmail.com>
2023-09-12 13:46:12 +0900

    commentFix

--- task/2-2/script/kadai5.vue
+++ task/2-2/script/kadai5.vue
@@ -1,5 +1,4 @@
 
-<!-- html側の表示(今回は未入力) -->
 <template>
     <div>
         <div class="error">未入力</div>
@@ -9,10 +8,8 @@
 <!-- script関連の記述 -->
 <script>
     module.exports = {
-        // vueのname定義(今回は'kadai5.vue')
         name: 'kadai5'
     }
-
 </script>
 
 <style scoped>
--- task/2-2/script/r05comp.vue
+++ task/2-2/script/r05comp.vue
@@ -1,26 +1,21 @@
-<!-- htmlの表示に関わること関連 -->
 <template>
     <div>
         <div class="divTitle">r05comp.vueです。</div>
         <div class="divMsg">{{ message }}</div>
     </div>
 </template>
-
-<!-- script関連 -->
 <script>
     module.exports = {
         data: function(){
             return {
-                message: "片岡先生"
+                message: "片岡"
             }
         }
     }
 </script>
 
-<!-- css関連 -->
 <style>
     .divTitle{
-        /* 文字色を赤に */
         color: #ff0000;
     }
 </style>
