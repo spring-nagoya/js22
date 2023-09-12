a5ad06a3e796d44795692d79110b7e66b901803e
sikigasa <km3t1114@yahoo.co.jp>
2023-09-12 09:14:31 +0900

    add: now_work

---
+++ class/2-2/script/r05comp.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <div class="divTitle">r05comp.vueです。</div>
+        <div>{{ message }}</div>
+    </div>
+</template>
+
+<script>
+module.exports = {
+    data: function () {
+        return {
+            message: "大嶋"
+        }
+    },
+}
+</script>
+
+<style>
+.divTitle {
+    font-size: 1.5em;
+    font-weight: bold;
+    color: #0000ff;
+    background-color: #ff0000;
+    padding: 10px;
+    margin: 10px;
+}
+
+.divMessage {
+    font-size: 1.5em;
+    font-weight: bold;
+    color: #0000ff;
+    background-color: #ff0000;
+    padding: 10px;
+    margin: 10px;
+}
+</style>
---
+++ class/2-2/script/script.js
@@ -0,0 +1,67 @@
+var topComp = '<div>TOPだよ</div>';
+var userComp = '<div>USERSです</div>';
+var btnComp = '<div>ボタン押した</div>';
+var errMsg = '<div>未入力</div>';
+
+//ルーティング定義
+var myRouter = new VueRouter(
+    {
+        routes: [
+            {
+                path: '/top',
+                component: {
+                    template: topComp
+                }
+            },
+            {
+                path: '/users',
+                component: {
+                    component: httpVueLoader('')
+                }
+            },
+            {
+                path: '/btn',
+                component: {
+                    template: btnComp
+                }
+            },
+            {
+                name: 'btnRoute',
+                path: '/btn/:userBtn',
+                component: {
+                    template: '<div>{{ $route.params.userBtn }}</div>'
+                }
+            },
+            {
+                path: '/error',
+                component: {
+                    template: errMsg
+                }
+            }
+
+        ]
+    });
+
+var app = new Vue(
+    {
+        router: myRouter,
+        data: {
+            inId: '',
+        },
+        methods: {
+            btnClick: function () {
+                if (this.inId == '') {
+                    //未入力
+                    this.$router.push({ path: '/error' });
+                } else {
+                    //入力あり
+                    this.$router.push(
+                        {
+                            name: 'btnRoute',
+                            path: '/btn',
+                            params: { userBtn: this.inId }
+                        });
+                }
+            }
+        }
+    }).$mount('#app');
